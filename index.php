89ae69a64d9de58dba9bf706059983a1764249a6
EmillyGuedes <emillyguedes.eg@gmail.com>
2020-07-07 16:35:12 -0300

    Descrição do autor, imagem e barra de navegação.

--- index.php
+++ index.php
@@ -3,9 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content= "palavras-chave,do,meu,site">
+    <meta name= "description" content="Descrição do meu website">
     <title>Projeto PHP</title>
 </head>
 <body>
+<header>
+<div class="logo">Logomarca</div><!--logo-->
+<nav class="desktop">
+<ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Serviços</a></li>
+        <li><a href="">Contato</a></li>
+</ul>
+</nav>
+
+<nav class="mobile">
+<ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Serviços</a></li>
+        <li><a href="">Contato</a></li>
+</ul>
+</nav>
+</header>
+<section class="banner-principal">
+<form>
+<h2>Qual o seu melhor e-mail?</h2>
+<input type="e-mail" name="e-mail" required />
+<input type="submite" name="acao" value="Cadastrar!">
+</form>
+
+</section><!banner-principal-->
+<section class="descricao-autor">
+<div class="w50">
+<h2>Emilly Guedes</h2>
+<p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div><!--w50-->
+<div class="w50">
+<!--pegar imagem depois-->
+<img src="img/perfil.jpg" />
+</div><!--w50-->
+</section><!--descricao-autor-->
+</body>
+</html>
+
+
+
+
+
+</header>
     
 </body>
 </html>
