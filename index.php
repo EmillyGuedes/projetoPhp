3ecde59020506c4c69286af61b4dff3860ccf62f
EmillyGuedes <emillyguedes.eg@gmail.com>
2020-07-07 17:14:48 -0300

    Especialidades, depoimentos, serviços e rodapé.[C

--- index.php
+++ index.php
@@ -1,52 +1,143 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content= "palavras-chave,do,meu,site">
-    <meta name= "description" content="Descrição do meu website">
+    <meta name="keywords" content="palavras-chave,do,meu,site">
+    <meta name="description" content="Descrição do meu website">
     <title>Projeto PHP</title>
 </head>
+
 <body>
-<header>
-<div class="logo">Logomarca</div><!--logo-->
-<nav class="desktop">
-<ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">Sobre</a></li>
-        <li><a href="">Serviços</a></li>
-        <li><a href="">Contato</a></li>
-</ul>
-</nav>
+    <header>
+        <div class="logo">Logomarca</div>
+        <!--logo-->
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
 
-<nav class="mobile">
-<ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">Sobre</a></li>
-        <li><a href="">Serviços</a></li>
-        <li><a href="">Contato</a></li>
-</ul>
-</nav>
-</header>
-<section class="banner-principal">
-<form>
-<h2>Qual o seu melhor e-mail?</h2>
-<input type="e-mail" name="e-mail" required />
-<input type="submite" name="acao" value="Cadastrar!">
-</form>
+        <nav class="mobile">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Sobre</a></li>
+                <li><a href="">Serviços</a></li>
+                <li><a href="">Contato</a></li>
+            </ul>
+        </nav>
+        </div>
+        <!--center-->
+    </header>
+    <section class="banner-principal">
+        <div class="center" <form>
+            <h2>Qual o seu melhor e-mail?</h2>
+            <input type="e-mail" name="e-mail" required />
+            <input type="submit" name="acao" value="Cadastrar!">
+            </form>
+        </div>
+        <!--center-->
 
-</section><!banner-principal-->
-<section class="descricao-autor">
-<div class="w50">
-<h2>Emilly Guedes</h2>
-<p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-</div><!--w50-->
-<div class="w50">
-<!--pegar imagem depois-->
-<img src="img/perfil.jpg" />
-</div><!--w50-->
-</section><!--descricao-autor-->
+    </section>
+    <!banner-principal-->
+        <section class="descricao-autor">
+            <div class="w50">
+                <h2>Emilly Guedes</h2>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <!--w50-->
+            <div class="w50">
+                <!--pegar imagem depois-->
+                <img src="img/perfil.jpg" />
+            </div>
+            <!--w50-->
+            </div>
+            <!--center-->
+        </section>
+        <!--descricao-autor-->
+        <section class="especialidades">
+            <div class="center">
+                <h2 class="title">Especialidades</h2>
+                <div class="box-especialidade">
+                    <h3>icone1</h3>
+                    <h3>CSS3</h3>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </p>
+                </div>
+                <!box-especialidade-->
+
+                    <div class="box-especialidade">
+                        <h3>icone2</h3>
+                        <h3>HTML5</h3>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </p>
+                    </div>
+                    <!box-especialidade-->
+
+                        <div class="box-especialidade">
+                            <h3>icone3</h3>
+                            <h3>JavaScript</h3>
+                            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </p>
+                        </div>
+                        <!box-especialidade-->
+
+            </div>
+            <!--center-->
+        </section><!--especialidades-->
+        <section class="extras">
+            <div class="center">
+                <div class="w50">
+                    <h2 class="title">Depoimentos</h2>
+                    <div class="depoimento-single">
+                        <p class="depoimento-descrição">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
+                        <p class="nome-autor">Lorem Ipsum</p>
+                    </div>
+                    <!--depoimento-single-->
+
+                    <div class="depoimento-single">
+                        <p class="depoimento-descrição">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
+                        <p class="nome-autor">Lorem Ipsum</p>
+                    </div><!--depoimento-single-->
+
+                    <div class="depoimento-single">
+                        <p class="depoimento-descrição">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
+                        <p class="nome-autor">Lorem Ipsum</p>
+                    </div><!--depoimento-single-->
+                </div>
+                <!--w50-->
+
+
+                <div class="w50">
+                    <h2 class="title">Serviços</h2>
+                    <div class="servicos">
+                        <ul>
+                            <li> orem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </li>
+
+                            <li> orem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </li>
+
+                            <li> orem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </li>
+
+                        </ul>
+                    </div>
+                    <!--servicos-->
+                </div>
+                <!--w50-->
+            </div>
+            <!--center-->
+        </section>
+        <!--extras-->
+
+        <footer>
+        <div class="center">
+        <p>Todos os direitos reservados</p>
+        </div>
+        
+        
+        </footer>
 </body>
+
 </html>
 
 
@@ -54,6 +145,7 @@
 
 
 </header>
-    
+
 </body>
+
 </html>
