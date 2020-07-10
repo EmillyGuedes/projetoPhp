49ecd4850dfb8f18fa8930666fcbb94ffc3d8554
EmillyGuedes <emillyguedes.eg@gmail.com>
2020-07-09 22:11:48 -0300

    Estilização do Header

--- index.php
+++ index.php
@@ -8,40 +8,43 @@
     <meta name="keywords" content="palavras-chave,do,meu,site">
     <meta name="description" content="Descrição do meu website">
     <link rel="stylesheet" href="style/font-awesome.min.css">
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="style/style.css">
 
 </head>
 
 <body>
     <header>
-        <div class="logo">Logomarca</div>
-        <!--logo-->
-        <nav class="desktop">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
+        <div class="center">
+            <div class="logo left">Logomarca</div>
+            <!--logo-->
+            <nav class="desktop right">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
 
-        <nav class="mobile">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Sobre</a></li>
-                <li><a href="">Serviços</a></li>
-                <li><a href="">Contato</a></li>
-            </ul>
-        </nav>
+            <nav class="mobile right">
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Sobre</a></li>
+                    <li><a href="">Serviços</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
         </div>
         <!--center-->
     </header>
     <section class="banner-principal">
-        <div class="center" <form>
-            <h2>Qual o seu melhor e-mail?</h2>
-            <input type="e-mail" name="e-mail" required />
-            <input type="submit" name="acao" value="Cadastrar!">
+        <div class="center">
+            <form>
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="e-mail" name="e-mail" required />
+                <input type="submit" name="acao" value="Cadastrar!">
             </form>
         </div>
         <!--center-->
@@ -49,16 +52,19 @@
     </section>
     <!banner-principal-->
         <section class="descricao-autor">
-            <div class="w50">
-                <h2>Emilly Guedes</h2>
-                <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="center">
+                <div class="w50 left">
+                    <h2>Emilly Guedes</h2>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
+                <!--w50-->
             </div>
-            <!--w50-->
-            <div class="w50">
+            <div class="w50 left">
                 <!--pegar imagem depois-->
                 <img src="img/perfil.jpg" />
             </div>
             <!--w50-->
+            <div class="clear"></div>
             </div>
             <!--center-->
         </section>
@@ -66,21 +72,21 @@
         <section class="especialidades">
             <div class="center">
                 <h2 class="title">Especialidades</h2>
-                <div class="box-especialidade">
+                <div class="w33 left box-especialidade">
                     <h3><i class="fa fa-css3" aria-hidden="true"></i></h3>
                     <h3>CSS3</h3>
                     <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </p>
                 </div>
                 <!box-especialidade-->
 
-                    <div class="box-especialidade">
+                    <div class="w33 left box-especialidade">
                         <h3><i class="fa fa-html5" aria-hidden="true"></i></h3>
                         <h3>HTML5</h3>
                         <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </p>
                     </div>
                     <!box-especialidade-->
 
-                        <div class="box-especialidade">
+                        <div class="w33 left box-especialidade">
                             <h3><i class="fa fa-gg-circle" aria-hidden="true"></i></h3>
                             <h3>JavaScript</h3>
                             <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. </p>
@@ -89,10 +95,11 @@
 
             </div>
             <!--center-->
-        </section><!--especialidades-->
+        </section>
+        <!--especialidades-->
         <section class="extras">
             <div class="center">
-                <div class="w50">
+                <div class="w50 left">
                     <h2 class="title">Depoimentos</h2>
                     <div class="depoimento-single">
                         <p class="depoimento-descrição">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
@@ -103,17 +110,19 @@
                     <div class="depoimento-single">
                         <p class="depoimento-descrição">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
                         <p class="nome-autor">Lorem Ipsum</p>
-                    </div><!--depoimento-single-->
+                    </div>
+                    <!--depoimento-single-->
 
                     <div class="depoimento-single">
                         <p class="depoimento-descrição">Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur.</p>
                         <p class="nome-autor">Lorem Ipsum</p>
-                    </div><!--depoimento-single-->
+                    </div>
+                    <!--depoimento-single-->
                 </div>
                 <!--w50-->
 
 
-                <div class="w50">
+                <div class="w50 left">
                     <h2 class="title">Serviços</h2>
                     <div class="servicos">
                         <ul>
@@ -128,17 +137,18 @@
                     <!--servicos-->
                 </div>
                 <!--w50-->
+                <div class="clear"></div>
             </div>
             <!--center-->
         </section>
         <!--extras-->
 
         <footer>
-        <div class="center">
-        <p>Todos os direitos reservados</p>
-        </div>
-        
-        
+            <div class="center">
+                <p>Todos os direitos reservados</p>
+            </div>
+
+
         </footer>
 </body>
 
