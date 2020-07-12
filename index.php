913fa5aeeb9c380fec9f2613f8d42a3c90003a94
EmillyGuedes <emillyguedes.eg@gmail.com>
2020-07-12 18:53:31 -0300

    Inserido favicon.

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Projeto PHP</title>
+    <link rel="shortcut icon" href="../install/favicon.ico" type="image/x-icon">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content="palavras-chave,do,meu,site">
