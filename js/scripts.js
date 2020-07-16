a2069f5157f12ae7d1437cbae3b4f74ee68c7085
EmillyGuedes <emillyguedes.eg@gmail.com>
2020-07-16 18:14:52 -0300

    Edição botão menu.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,16 @@ $(function () {
     // Aqui ira todo o nosso script em JavaScript
     $('nav.mobile').click(function(){
         var listaMenu = $('nav.mobile ul');
-        /* if(listaMenu.is(':hidden') == true)
-        listaMenu.fadeIn();
-        else
-        listaMenu.fadeOut() */
-
+        
+        if(listaMenu.is(':hidden') == true){
+        var icone = $('.botao-menu-mobile').find('i');
+        icone.removeClass('fa-bars');
+        icone.addClass('fa-times');
+        listaMenu.slideToggle();
+         } 
+         else{
          listaMenu.slideToggle();
+
+         }
     })
 })
